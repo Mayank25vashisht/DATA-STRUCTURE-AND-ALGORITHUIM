200990c68290820351f02e6ee824fbf83b28fa1b
Mayank25vashisht <mvashisht_be24@thapar.edu>
2025-08-18 20:27:16 +0530

    Add files via upload

---
+++ transpose.cpp
@@ -0,0 +1,22 @@
+# include <iostream>
+using namespace std;
+int transpose(int arr[][3],int rows,int columns){
+    for(int i=0;i<columns;i++){
+        for(int j=0;j<rows;j++){
+            cout<<arr[j] [i];
+        }cout<<endl;
+    }
+}
+int main(){
+    int arr[2] [3]={{1,2,3},{2,3,3}};
+    
+    int rows=2;
+    int columns=3;
+    for(int i=0;i<rows;i++){
+        for(int j=0;j<columns;j++){
+            cout<<arr[i][j];
+        } cout<<endl;
+    }
+    transpose(arr,rows,columns);
+    return 0;
+}
